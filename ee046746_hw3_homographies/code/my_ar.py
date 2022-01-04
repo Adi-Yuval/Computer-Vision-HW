353ff822056ac7494d0227a081161380be6d03b6
yuvalgos <yuvalgos@gmail.com>
2022-01-04 12:03:30 +0200

    started implementing function to cut books

--- ee046746_hw3_homographies/code/my_ar.py
+++ ee046746_hw3_homographies/code/my_ar.py
@@ -4,11 +4,14 @@ import cv2
 import scipy
 from matplotlib import pyplot as plt
 import my_homography as mh
+
+
 #Add imports if needed:
 """
 Your code here
 """
 #end imports
+
 
 #Add functions here:
 """
@@ -16,12 +19,36 @@ Your code here
 """
 #Functions end
 
+
 # HW functions:
 def create_ref(im_path):
     """
        Your code here
     """
+
+    im = cv2.cvtColor(cv2.imread(im_path), cv2.COLOR_BGR2RGB)
+    plt.imshow(im)
+    # mark points from top left clockwise
+    book_corners = np.array(plt.ginput(4, timeout=-1))
+    plt.close()
+
+    target_width = int(np.maximum(np.linalg.norm(book_corners[1] - book_corners[0]),
+                                  np.linalg.norm(book_corners[2] - book_corners[3])))
+    target_height = int(np.maximum(np.linalg.norm(book_corners[2] - book_corners[1]),
+                                   np.linalg.norm(book_corners[3] - book_corners[0])))
+
+    target_corners = np.array([[0, 0], [target_width, 0], [target_width, target_height], [0, target_height]])
+
+    H = mh.computeH(book_corners.T, target_corners.T)
+    # H = np.linalg.inv(H)
+
+    ref_image, _ = mh.warpH(im, H)
+    # ref_image = cv2.warpPerspective(im, H, (target_width, target_height))
     return ref_image
 
 if __name__ == '__main__':
     print('my_ar')
+
+    ref_image = create_ref("my_data/my_image1.jpg")
+    plt.imshow(ref_image)
+    plt.show()
